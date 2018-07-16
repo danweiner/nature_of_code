0fed794771e6bdad56a430c7ee598168a93f5860
Dan Weiner <daweiner16@gmail.com>
2018-07-16 12:31:03 -0400

    add random walker

---
+++ random_walker/sketch.js
@@ -0,0 +1,39 @@
+var w;
+
+function setup() {
+	createCanvas(640, 360);
+	w = new Walker();
+	background(255);
+	frameRate(100);
+}
+
+function draw() {
+	w.step();
+	w.display();
+
+}
+
+class Walker {
+	constructor(x, y) {
+		this.x = width/2;
+		this.y = height/2;
+	}
+
+	display() {
+		stroke(0);
+		point(this.x, this.y);
+	}
+
+	step() {
+		var choice = int(random(4)); // 0, 1, 2 or 3
+		if (choice == 0) {
+			this.x++;
+		} else if (choice == 1) {
+			this.x--;
+		} else if (choice == 2) {
+			this.y++;
+		} else {
+			this.y--;
+		}
+	}
+}
