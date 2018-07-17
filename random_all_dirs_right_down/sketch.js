15e6cb659896ac00bbb3e234f14e537e01fa3920
Dan Weiner <daweiner16@gmail.com>
2018-07-17 08:47:15 -0400

    add random walker with decimals moves all dirs

---
+++ random_all_dirs_right_down/sketch.js
@@ -0,0 +1,37 @@
+var w;
+
+function setup() {
+	createCanvas(640, 360);
+	w = new Walker();
+	background(255);
+	frameRate(100);
+}
+
+function draw() {
+	w.step();
+	w.display();
+	//console.log(w);
+
+}
+
+class Walker {
+	constructor(x, y) {
+		this.x = width/2;
+		this.y = height/2;
+	}
+
+	display() {
+		stroke(0);
+		point(this.x, this.y);
+	}
+
+	step() {
+		var stepx = random(-.97, 1);
+		var stepy = random(-.97, 1);
+		console.log(stepx);
+		console.log(stepy);
+
+		this.x += stepx;
+		this.y += stepy;
+	}
+}
